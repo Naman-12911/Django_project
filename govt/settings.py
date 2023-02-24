67b8351803185a2703b3c8599354dafcce50573c
Naman-12911 <namangarg820@gmail.com>
2023-02-24 19:21:18 +0530

    version

--- govt/settings.py
+++ govt/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 
 
 
-ALLOWED_HOSTS = [".vercel.app", "localhost"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', "localhost"]
 
 
 # Application definition
@@ -159,4 +159,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "namangarg820@gmail.com"
-EMAIL_HOST_PASSWORD = "mfpecfbzillvzskc"
+EMAIL_HOST_PASSWORD = ""
